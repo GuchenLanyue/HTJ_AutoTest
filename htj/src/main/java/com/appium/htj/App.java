768e5e397c3c51140cee5eb4a9997ece9fabbf1f
houlong <sam.hou@microfastup.com>
2016-06-20 17:02:27 +0800

    黄太吉自动化测试项目

---
+++ htj/src/main/java/com/appium/htj/App.java
@@ -0,0 +1,13 @@
+package com.appium.htj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
