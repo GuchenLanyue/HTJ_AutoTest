dfbf2f59c218aa4d93f82ab2d8a7411f2aa80e38
houlong <sam.hou@microfastup.com>
2016-06-21 15:53:56 +0800

    怎加login界面

--- htj/src/test/java/com/appium/htj/pages/Guide.java
+++ htj/src/test/java/com/appium/htj/pages/Guide.java
@@ -7,21 +7,7 @@ import org.openqa.selenium.By;
  * */
 
 public class Guide {
-//	/**搜索按钮*/
-//	 public static final By HP_BUTTON_SEARCH = By.id("com.microsoft.bing:id/search"); 
-//	/**更多内容按钮*/
-//	 public static final By HP_BUTTON_MORE = By.id("com.microsoft.bing:id/search_homepage_hamburger"); 
-//	/**图片按钮*/
-//	 public static final By HP_BUTTON_IMAGE = By.id("com.microsoft.bing:id/hp_scope_images"); 
-//	/**视频按钮*/
-//	 public static final By HP_BUTTON_VDEDIO = By.id("com.microsoft.bing:id/hp_scope_videos"); 
-//	/**学术按钮*/
-//	 public static final By HP_BUTTON_ACADEMIC = By.id("com.microsoft.bing:id/hp_scope_academic"); 
-//	/**词典按钮*/
-//	 public static final By HP_BUTTON_DICT = By.id("com.microsoft.bing:id/hp_scope_dict");
-//	 /**图片故事*/
-//	 public static final By HP_LINK_STORY = By.id("com.microsoft.bing:id/opal_home_picture_story_text1");
-	 
+
 	 /**引导页ViewPager*/
 	 public static final By Guide_image_pager = By.id("com.huangtaiji.client:id/vPager");
 	 
